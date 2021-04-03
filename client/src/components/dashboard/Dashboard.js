a931954918349815929ad6cc839c72211ad123f8
DESKTOP-HB65II7\Justin <jroy@sociusmarketing.com>
2021-04-03 14:57:15 -0400

    Add CLEAR_PROFILE action Start Dashboard Create auth-only nav links

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profile'
+import Spinner from '../layout/Spinner'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+    return loading && profile === null ? <Spinner /> : <>
+        <h1 className="large text-primary">Dashbaord</h1>
+        <p className="lead"><i className="fas fa-user"></i> Welcome {user && user.name}</p>
+        {profile !== null ? <>Has</> : <>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+                Create Profile
+        </Link>
+        </>}
+    </>;
 }
 
 Dashboard.propTypes = {
