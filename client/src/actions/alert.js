1304535ef31af7c59e2215d39cf0ca1ddf9b472d
DESKTOP-HB65II7\Justin <justinroy813@gmail.com>
2021-04-08 08:03:11 -0400

    Comlete add profile functionality

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType) => dispatch => {
-    const id = uuid.v4();
+    const id = uuid();
     dispatch({
         type: SET_ALERT,
         payload: { msg, alertType, id }
